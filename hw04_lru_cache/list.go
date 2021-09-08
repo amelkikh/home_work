bb5ead6f657db1ae30c26c604f767c35c45d62b1
Aleksandr Melkikh <aleksandr.melkih@gs-labs.ru>
2021-09-08 02:56:24 +0200

    hw04 done

--- hw04_lru_cache/list.go
+++ hw04_lru_cache/list.go
@@ -14,11 +14,89 @@ type ListItem struct {
 	Value interface{}
 	Next  *ListItem
 	Prev  *ListItem
+	list  *list
 }
 
 type list struct {
-	List // Remove me after realization.
-	// Place your code here.
+	head, tail *ListItem
+	count      int
+}
+
+func (l list) Len() int {
+	return l.count
+}
+
+func (l list) Front() *ListItem {
+	return l.head
+}
+
+func (l list) Back() *ListItem {
+	return l.tail
+}
+
+func (l *list) PushFront(v interface{}) *ListItem {
+	item := &ListItem{
+		Value: v,
+		Next:  l.head,
+		list:  l,
+	}
+	if l.head != nil {
+		l.head.Prev = item
+	} else {
+		l.tail = item
+	}
+	l.head = item
+	l.count++
+
+	return item
+}
+
+func (l *list) PushBack(v interface{}) *ListItem {
+	item := &ListItem{
+		Value: v,
+		Prev:  l.tail,
+		list:  l,
+	}
+	l.tail.Next = item
+	l.tail = item
+	l.count++
+
+	return item
+}
+
+func (l *list) Remove(item *ListItem) {
+	if item == nil || item.list == nil || item.list != l {
+		return
+	}
+	if item.Prev != nil {
+		item.Prev.Next = item.Next
+	} else {
+		l.head = item.Next
+	}
+	if item.Next != nil {
+		item.Next.Prev = item.Prev
+	} else {
+		l.tail = item.Prev
+	}
+	// avoid memory leaks
+	item.Next = nil
+	item.Prev = nil
+	// unlink item to avoid incorrect behaviour with other operations with list
+	item.list = nil
+	l.count--
+}
+
+func (l *list) MoveToFront(item *ListItem) {
+	if item.Prev == nil || item.list == nil {
+		// Don't need to move front first item or if item unlinked (deleted) from list
+		return
+	}
+	l.Remove(item)
+	l.head.Prev = item
+	item.Next = l.head
+	l.head = item
+	item.list = l
+	l.count++
 }
 
 func NewList() List {
