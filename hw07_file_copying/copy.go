c857b81d6d5a324fca34988849fd9e315287f102
Aleksandr Melkikh <aleksandr.melkih@gs-labs.ru>
2021-09-12 02:59:09 +0200

    hw07 file copying done

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -1,15 +1,95 @@
 package main
 
 import (
+	"bufio"
 	"errors"
+	"io"
+	"os"
+	"time"
+
+	"github.com/cheggaaa/pb/v3"
 )
 
 var (
 	ErrUnsupportedFile       = errors.New("unsupported file")
 	ErrOffsetExceedsFileSize = errors.New("offset exceeds file size")
+	ErrNegativeValue         = errors.New("negative value is unacceptable")
 )
 
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
+	info, err := os.Stat(fromPath)
+	if err != nil {
+		return err
+	}
+	if limit < 0 || offset < 0 {
+		return ErrNegativeValue
+	}
+	if info.Size() == 0 {
+		return ErrUnsupportedFile
+	}
+	if offset > info.Size() {
+		return ErrOffsetExceedsFileSize
+	}
+	if limit == 0 {
+		limit = info.Size() - offset
+	}
+
+	f, err := os.Open(fromPath)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	fTo, err := os.Create(toPath)
+	if err != nil {
+		return err
+	}
+	defer fTo.Close()
+
+	r := bufio.NewReader(f)
+	if offset > 0 {
+		_, err = r.Discard(int(offset))
+		if err != nil {
+			return err
+		}
+	}
+	w := bufio.NewWriter(fTo)
+	buf := make([]byte, 1024)
+
+	bar := pb.StartNew(int(limit))
+	bar.Set(pb.Bytes, true)
+	defer bar.Finish()
+
+	var n int64 = 0
+	for n <= limit {
+		nn, err := r.Read(buf)
+		if err != nil && !errors.Is(err, io.EOF) {
+			return err
+		}
+
+		if nn == 0 {
+			break
+		}
+
+		if n+int64(nn) > limit {
+			nn = int(limit - n)
+			n = limit
+		} else {
+			n += int64(nn)
+		}
+
+		if _, err := w.Write(buf[:nn]); err != nil {
+			return err
+		}
+
+		bar.Add(nn)
+		// Just for testing purposes to visualize progress
+		time.Sleep(time.Millisecond * 300)
+	}
+
+	if err = w.Flush(); err != nil {
+		return err
+	}
+
 	return nil
 }
--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,128 @@
 package main
 
-import "testing"
+import (
+	"bytes"
+	"io/fs"
+	"io/ioutil"
+	"math/rand"
+	"os"
+	"strconv"
+	"strings"
+	"testing"
+	"time"
 
-func TestCopy(t *testing.T) {
-	// Place your code here.
+	"github.com/stretchr/testify/suite"
+)
+
+type CopyDataSuite struct {
+	suite.Suite
+	f *os.File
+	b []byte
+}
+
+func TestCopyDataSuite(t *testing.T) {
+	suite.Run(t, new(CopyDataSuite))
+}
+
+func generateFilename() string {
+	return strings.Join([]string{os.TempDir(), "otus_out" + strconv.Itoa(rand.Int())}, string(os.PathSeparator))
+}
+
+func (c *CopyDataSuite) SetupSuite() {
+	var err error
+	c.f, err = ioutil.TempFile("", "otus_copy")
+	c.Require().NoError(err)
+
+	bigBuff := make([]byte, 2<<9)
+	_, err = rand.Read(bigBuff)
+	c.Require().NoError(err)
+
+	_, err = c.f.Write(bigBuff)
+	c.Require().NoError(err)
+
+	c.b = bigBuff
+
+	err = c.f.Close()
+	c.Require().NoError(err)
+
+	rand.Seed(time.Now().Unix())
+}
+
+func (c *CopyDataSuite) TearDownSuite() {
+	err := os.Remove(c.f.Name())
+	c.Require().NoError(err)
+}
+
+func (c *CopyDataSuite) TestOffsetExceeded() {
+	err := Copy(c.f.Name(), "", 2<<9+1, 0)
+	c.Require().ErrorIs(err, ErrOffsetExceedsFileSize)
+}
+
+func (c *CopyDataSuite) TestCopyPartFile() {
+	tmpName := generateFilename()
+	err := Copy(c.f.Name(), tmpName, 0, 10)
+	c.Require().NoError(err)
+
+	f2, err := os.Open(tmpName)
+	data2, err := ioutil.ReadAll(f2)
+	c.Require().NoError(err)
+
+	c.Require().True(bytes.Compare(c.b[:10], data2) == 0)
+
+	err = os.Remove(tmpName)
+	c.Require().NoError(err)
+}
+
+func (c *CopyDataSuite) TestCopyFileOffset() {
+	tmpName := generateFilename()
+	err := Copy(c.f.Name(), tmpName, 10, 10)
+	c.Require().NoError(err)
+
+	f2, err := os.Open(tmpName)
+	data2, err := ioutil.ReadAll(f2)
+	c.Require().NoError(err)
+
+	c.Require().True(bytes.Compare(c.b[10:20], data2) == 0)
+
+	err = os.Remove(tmpName)
+	c.Require().NoError(err)
+}
+
+func (c *CopyDataSuite) TestCopyFullFile() {
+	tmpName := generateFilename()
+	err := Copy(c.f.Name(), tmpName, 0, 0)
+	c.Require().NoError(err)
+
+	f2, err := os.Open(tmpName)
+	data2, err := ioutil.ReadAll(f2)
+	c.Require().NoError(err)
+
+	c.Require().True(bytes.Compare(c.b, data2) == 0)
+
+	err = os.Remove(tmpName)
+	c.Require().NoError(err)
+}
+
+func (c *CopyDataSuite) TestErrorOffset() {
+	tmpName := generateFilename()
+	err := Copy(c.f.Name(), tmpName, -1, 0)
+	c.Require().ErrorIs(err, ErrNegativeValue)
+}
+
+func (c *CopyDataSuite) TestErrorInvalidFile() {
+	err := Copy("/dev/random", "", 0, 0)
+	c.Require().ErrorIs(err, ErrUnsupportedFile)
+}
+
+func (c *CopyDataSuite) TestErrorCopyFile() {
+	var pathErr *fs.PathError
+	tmpName := "test"
+	err := Copy(tmpName, tmpName, 0, 0)
+	c.Require().ErrorAs(err, &pathErr)
+}
+
+func (c *CopyDataSuite) TestErrorLimit() {
+	tmpName := generateFilename()
+	err := Copy(c.f.Name(), tmpName, 0, -1)
+	c.Require().ErrorIs(err, ErrNegativeValue)
 }
